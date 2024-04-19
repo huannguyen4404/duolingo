2fddda9738e17d69cd842ecc7f4dc6791152bf58
huan <huannguyen4404@gmail.com>
2024-04-19 10:23:49 +0700

    AI voice, useKey and challenge footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -2,9 +2,10 @@
 
 import { challengeOptions, challenges } from '@/db/schema'
 import { useState } from 'react'
+import { Challenge } from './challenge'
+import { Footer } from './footer'
 import { Header } from './header'
 import { QuestionBubble } from './question-bubble'
-import { Challenge } from './challenge'
 
 type Props = {
   initialPercentage: number
@@ -33,8 +34,18 @@ export const Quiz = ({
     )
     return uncompletedIndex === -1 ? 0 : uncompletedIndex
   })
+
+  const [selectedOption, setSelectedOption] = useState<number>()
+  const [status, setStatus] = useState<'correct' | 'wrong' | 'none'>('none')
+
   const challenge = challenges[activeIndex]
   const options = challenge?.challengeOptions || []
+
+  const onSelect = (id: number) => {
+    if (status !== 'none') return
+
+    setSelectedOption(id)
+  }
 
   const title =
     challenge.type === 'ASSIST'
@@ -51,7 +62,7 @@ export const Quiz = ({
 
       <div className="flex-1">
         <div className="h-full flex items-center justify-center">
-          <div className="lg:min-h-[35px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
             <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
               {title}
             </h1>
@@ -62,9 +73,9 @@ export const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
+                onSelect={onSelect}
                 status="none"
-                selectedOption={undefined}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -72,6 +83,7 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   )
 }
